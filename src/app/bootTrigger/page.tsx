105baf32a45ac49ff159f60e039e14e43889859e
Roman Torres <rtorres@envivent.com>
2024-04-21 19:55:41 -0700

    intial interation of boot sequence

---
+++ src/app/bootTrigger/page.tsx
@@ -0,0 +1,105 @@
+'use client'
+
+import Image from 'next/image'
+
+// Images
+import joinVaulttec from '../assets/images/join.png';
+
+export default function Page() {
+  const startBootSequence = () => {
+    const landing: HTMLElement | null = document.querySelector('.step-0');
+    const stepOne: HTMLElement | null = document.querySelector('.step-1');
+
+    if (landing) {
+      landing.classList.add('hide');
+    }
+    if (stepOne) {
+      stepOne.classList.toggle('hide');
+      // Get bootloader entries to remove class hide - display individual boot entry
+      const bootloadEntries = document.querySelectorAll<HTMLElement>('.bootload-entry');
+
+      for (let index = 0; index < bootloadEntries.length; index++) {
+        const bootloadEntry = bootloadEntries[index];
+        setTimeout(() => {
+          bootloadEntry.classList.toggle('hide');
+
+          const specStatus = bootloadEntry.querySelector('.bootload-spec-status');
+
+          if (specStatus) {
+            setTimeout(() => {
+              specStatus.classList.toggle('hide');
+            }, 200);
+          }
+
+          if (index === bootloadEntries.length - 1) {
+            // set the end status for the step 1 animation here..
+            setTimeout(() => {
+              const stepOne = document.querySelector('.step-1') as HTMLElement;
+              if (stepOne) {
+                stepOne.classList.toggle('hide');
+                sequenceTwo();
+              }
+            }, 100);
+          }
+
+          bootloadEntry.scrollIntoView();
+        }, 10 * index);
+      }
+
+    }
+  };
+  const sequenceTwo = () => {
+    // Using animationend event listener to mark the end of animation
+    const stepTwo = document.querySelector('.step-2') as HTMLElement;
+
+    stepTwo.classList.toggle("hide");
+
+    const targetNode = document.querySelector<HTMLElement>(".os-load-rom");
+    const animationEnd = () => {
+      // Set the end state of animation for step 2 here...
+      setTimeout(() => {
+        stepTwo.classList.toggle("hide");
+        sequenceThree();
+      }, 500);
+    };
+
+    if (targetNode) {
+      targetNode.addEventListener("animationend", animationEnd);
+    }
+
+  };
+  const sequenceThree = () => {
+    const stepThree = document.querySelector('.step-3') as HTMLElement;
+
+    stepThree.classList.toggle('hide');
+
+    const setTimerforpipboy = () => {
+      setTimeout(() => {
+        stepThree.classList.toggle('hide');
+        sequenceFour();
+      }, 2200);
+    };
+
+    setTimerforpipboy();
+  };
+  const sequenceFour = () => {
+    const stepFour = document.querySelector(".step-4");
+
+    if (stepFour) {
+      stepFour.classList.toggle("hide");
+    }
+  };
+
+  return (
+    <div className="step-0">
+      <Image
+        src={joinVaulttec}
+        alt="Join the Vault-tec"
+        className="join-vaulttec-img"
+        priority
+      />
+      <button className="crt join-vaulttec-btn"
+        onClick={startBootSequence}>JOIN</button>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,68 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
+import { useEffect } from 'react'
+import Image from 'next/image'
+
+// Images
+import pipboyWalking from './assets/images/walking-pipboy.gif';
+
+export default function Page() {
+  useEffect(() => {
+
+  }, [])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="navbar crt">
+        <ul className="navbar-main">
+          <li><a href="#">STAT</a></li>
+          <li><a href="#">INV</a></li>
+          <li><a href="#">DATA</a></li>
+          <li><a href="#">MAP</a></li>
+          <li><a href="#">RADIO</a></li>
+        </ul>
+      </div>
+      <div className="main-content">
+        <ul className="navbar-secondary">
+          <li>STATUS</li>
+          <li>SPECIAL</li>
+          <li>PERKS</li>
+        </ul>
+        <div className="walking-pipboy-container">
+          <Image
+            src={pipboyWalking}
+            alt="Pipboy walking"
+            className="pipboy-walking"
+            priority
+          />
+        </div>
+        <div className="footer">
+          <ul className="crt navbar-buttons">
+            <li>STIMPAK(1)</li>
+            <li>RADAWAY(0)</li>
+          </ul>
+          <div className="indicators-container">
+            <div className="health-indicator">
+              <ul>
+                <li>HP</li>
+                <li>57/135</li>
+              </ul>
+            </div>
+            <div className="level-indicator">
+              <ul>
+                <li>LEVEL 6</li>
+                <li className="progress-bar-container"><span className="progress-bar"></span></li>
+              </ul>
+            </div>
+            <div className="ammo-indicator">
+              <ul>
+                <li>AP</li>
+                <li>90/90</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
